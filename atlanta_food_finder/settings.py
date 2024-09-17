9987b229c534719391f1a43c3c1fba1720f1d811
Manan Khanna <manankhanna2005@gmail.com>
2024-09-17 16:35:46 -0400

    tried implementing reset password, cant get my own template to work though

--- atlanta_food_finder/settings.py
+++ atlanta_food_finder/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'team362340@gmail.com'
+EMAIL_HOST_PASSWORD = 'Group36@123'
