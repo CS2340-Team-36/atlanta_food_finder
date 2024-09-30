a2090f9da1386426a4dfd5a13820536ae3da5b42
Manan Khanna <manankhanna2005@gmail.com>
2024-09-29 22:36:04 -0400

    removed restaurant details

--- atlanta_food_finder/settings.py
+++ atlanta_food_finder/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurants',
-    'restaurant_details',
     'user_accounts'
 
 ]
--- atlanta_food_finder/urls.py
+++ atlanta_food_finder/urls.py
@@ -21,6 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurants/', include('restaurants.urls')),
-    path("restaurant/", include("restaurant_details.urls")),
     path("accounts/", include("user_accounts.urls")),
 ]
